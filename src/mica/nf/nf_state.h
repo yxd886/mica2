5a42bb3356079d239921980cdf48a1823b5b053e
xiaodongyi <409070169@qq.com>
2017-10-06 13:00:45 +0800

    change directory format

--- src/mica/nf/nf_state.h
+++ src/mica/nf/nf_state.h
@@ -7,22 +7,22 @@
 
 struct firewall_state{
     uint8_t _tcp_flags;
-	uint32_t _sent_seq;
-	uint32_t _recv_ack;
-	bool _pass;
+    uint32_t _sent_seq;
+    uint32_t _recv_ack;
+    bool _pass;
 
-	firewall_state():_tcp_flags(0),_sent_seq(0),_recv_ack(0),_pass(true){
+    firewall_state():_tcp_flags(0),_sent_seq(0),_recv_ack(0),_pass(true){
 
-	}
-	firewall_state(uint8_t tcp_flags,uint32_t sent_seq,uint32_t recv_ack):_tcp_flags(tcp_flags),_sent_seq(sent_seq),_recv_ack(recv_ack),_pass(true){
+    }
+    firewall_state(uint8_t tcp_flags,uint32_t sent_seq,uint32_t recv_ack):_tcp_flags(tcp_flags),_sent_seq(sent_seq),_recv_ack(recv_ack),_pass(true){
 
-	}
-	void copy(struct firewall_state* c){
-	    _tcp_flags=c->_tcp_flags;
-		_sent_seq=c->_sent_seq;
-		_recv_ack=c->_recv_ack;
-		_pass=c->_pass;
-	}
+    }
+    void copy(struct firewall_state* c){
+        _tcp_flags=c->_tcp_flags;
+        _sent_seq=c->_sent_seq;
+        _recv_ack=c->_recv_ack;
+        _pass=c->_pass;
+    }
 
 
 };
@@ -30,14 +30,11 @@ struct firewall_state{
 
 
 struct session_state{
-	uint32_t _action;
+    uint32_t _action;
 
-	//firewall state:
-	struct firewall_state _firewall_state;
-
-
-
-	session_state():_action(READ),_firewall_state(){}
+    //firewall state:
+    struct firewall_state _firewall_state;
+    session_state():_action(READ),_firewall_state(){}
 
 };
 
