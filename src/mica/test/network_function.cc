9ffe41882f71eb7d83edb90b914cef074289a79e
xiaodongyi <409070169@qq.com>
2017-10-06 15:09:57 +0800

    load_balancer

--- src/mica/test/network_function.cc
+++ src/mica/test/network_function.cc
@@ -6,6 +6,7 @@
 #include <vector>
 #include <iostream>
 #include "mica/nf/firewall.h"
+#include "mica/nf/load_balancer.h"
 
 
 int main(int argc, const char* argv[]) {
@@ -13,5 +14,6 @@ int main(int argc, const char* argv[]) {
 	struct rte_ring* worker2interface[10];
 	struct rte_ring* interface2worker[10];
 	Firewall a(worker2interface,interface2worker);
+	Load_balancer b(worker2interface,interface2worker,0);
 	return 0;
 }
