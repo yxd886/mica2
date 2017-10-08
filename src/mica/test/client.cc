42b7f51ec1d7702d5704d93fed8415dd3bf6a3a1
xiaodongyi <409070169@qq.com>
2017-10-08 16:46:40 +0800

    client.cc

--- src/mica/test/client.cc
+++ src/mica/test/client.cc
@@ -897,11 +897,44 @@ void port_config(){
     port_conf.txmode.mq_mode = ETH_MQ_TX_NONE;
 }
 
+//for mica
+
+struct DPDKConfig : public ::mica::network::BasicDPDKConfig {
+  static constexpr bool kVerbose = true;
+};
+
+struct DatagramClientConfig
+    : public ::mica::datagram::BasicDatagramClientConfig {
+  typedef ::mica::network::DPDK<DPDKConfig> Network;
+  // static constexpr bool kSkipRX = true;
+  // static constexpr bool kIgnoreServerPartition = true;
+  // static constexpr bool kVerbose = true;
+};
+
+typedef ::mica::datagram::DatagramClient<DatagramClientConfig> Client;
+
+typedef ::mica::table::Result Result;
+
+template <typename T>
+static uint64_t hash(const T* key, size_t key_length) {
+  return ::mica::util::hash(key, key_length);
+}
+
+class ResponseHandler
+    : public ::mica::datagram::ResponseHandlerInterface<Client> {
+ public:
+  void handle(Client::RequestDescriptor rd, Result result, const char* value,
+              size_t value_length, const Argument& arg) {
+    (void)rd;
+    (void)result;
+    (void)value;
+    (void)value_length;
+    (void)arg;
+  }
+};
 
 
-/*class mica_client{
 
-};*/
 
 int
 main(int argc, char **argv)
@@ -1111,6 +1144,85 @@ main(int argc, char **argv)
 
 
     //start mica client
+    auto config = ::mica::util::Config::load_file("client.json");
+
+      DatagramClientConfig::Network network(config.get("network"));
+      network.start();
+      Client::DirectoryClient dir_client(config.get("dir_client"));
+
+      Client client(config.get("client"), &network, &dir_client);
+      client.discover_servers();
+      client.probe_reachability();
+
+        ResponseHandler rh;
+
+        size_t num_items = 192 * 1048576;
+
+        // double get_ratio = 0.95;
+        double get_ratio = 0.50;
+
+        uint32_t get_threshold = (uint32_t)(get_ratio * (double)((uint32_t)-1));
+
+        ::mica::util::Rand op_type_rand(static_cast<uint64_t>(args->lcore_id) + 1000);
+        ::mica::util::ZipfGen zg(num_items, args->zipf_theta,
+                                 static_cast<uint64_t>(args->lcore_id));
+        ::mica::util::Stopwatch sw;
+        sw.init_start();
+        sw.init_end();
+
+        uint64_t key_i;
+        uint64_t key_hash;
+        size_t key_length = sizeof(key_i);
+        char* key = reinterpret_cast<char*>(&key_i);
+
+        uint64_t value_i;
+        size_t value_length = sizeof(value_i);
+        char* value = reinterpret_cast<char*>(&value_i);
+
+        bool use_noop = false;
+        // bool use_noop = true;
+
+        uint64_t last_handle_response_time = sw.now();
+        // Check the response after sending some requests.
+        // Ideally, packets per batch for both RX and TX should be similar.
+        uint64_t response_check_interval = 20 * sw.c_1_usec();
+
+        uint64_t seq = 0;
+        while (true) {
+          // Determine the operation type.
+          uint32_t op_r = op_type_rand.next_u32();
+          bool is_get = op_r <= get_threshold;
+
+          // Generate the key.
+          key_i = zg.next();
+          key_hash = hash(key, key_length);
+
+          uint64_t now = sw.now();
+          while (!client.can_request(key_hash) ||
+                 sw.diff_in_cycles(now, last_handle_response_time) >=
+                     response_check_interval) {
+            last_handle_response_time = now;
+            client.handle_response(rh);
+          }
+
+          if (!use_noop) {
+            if (is_get)
+              client.get(key_hash, key, key_length);
+            else {
+              value_i = seq;
+              client.set(key_hash, key, key_length, value, value_length, true);
+            }
+          } else {
+            if (is_get)
+              client.noop_read(key_hash, key, key_length);
+            else {
+              value_i = seq;
+              client.noop_write(key_hash, key, key_length, value, value_length);
+            }
+          }
+
+          seq++;
+        }
 
 
 
