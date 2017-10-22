cfa3de33e81d7d2e3cba481bb469b533bc347df9
yxd886 <409070169@qq.com>
2017-10-22 11:11:31 +0800

    A

--- src/mica/nf/ips.h
+++ src/mica/nf/ips.h
@@ -69,6 +69,7 @@ void process_batch(const struct aho_dfa *dfa_arr,
 }
 
 bool state_updated(struct ips_state* old_,struct ips_state* new_){
+	if(DEBUG) printf("old_->_alert:%d new_->_alert:%d old_->_dfa_id:%d new_->_dfa_id:%d old_->_state:%d new_->_state:%d",old_->_alert,new_->_alert,old_->_dfa_id,new_->_dfa_id,old_->_state,new_->_state);
 	if(old_->_alert==new_->_alert&&old_->_dfa_id==new_->_dfa_id&&old_->_state==new_->_state){
 		return false;
 	}
